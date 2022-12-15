7d1b6136139c2615eb3125fb0da79c3901202d54
N. Cox <120606330+ncox8591@users.noreply.github.com>
2022-12-15 17:43:04 -0500

    Add files via upload

---
+++ Intro to Computers, Programs, and Java/Exercise 1.2/Exercise1_2.java
@@ -0,0 +1,27 @@
+/* Intro to Java Programming and Data Structures by Y. Daniel Liang
+ * Chapter 1 Introduction to Computers, Programs, and Java
+ * 
+ * Exercise 1.2 (Display five messages) Write a program that displays Welcome
+ * to Java five times.
+ */
+
+package exercise1_2;
+
+//Class Definition and Block
+public class Exercise1_2 {
+
+    //Main Method Block
+    public static void main(String[] args) {
+
+        //Solution Option 1
+        // Statements to display the strings
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        
+
+    } //End Main Method Block
+    
+} //End Class Block
