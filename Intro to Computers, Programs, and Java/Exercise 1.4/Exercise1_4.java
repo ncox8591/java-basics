c58ff33c7231a0d2b3c59ab70a7ce7d6881dbe4d
N. Cox <120606330+ncox8591@users.noreply.github.com>
2022-12-15 20:42:52 -0500

    Add files via upload

---
+++ Intro to Computers, Programs, and Java/Exercise 1.4/Exercise1_4.java
@@ -0,0 +1,28 @@
+/* Intro to Java Programming and Data Structures by Y. Daniel Liang
+ * Chapter 1 Introduction to Computers, Programs, and Java
+ * 
+ * Exercise 1.4 (Print a table) Write a program that displays the following
+ * table:
+ * 
+ * a   a^2   a^3
+ * 1    1     1
+ * 2    4     8
+ * 3    9     27
+ * 4    16    64
+ */
+package exercise1_4;
+
+//Class Definition and Block
+public class Exercise1_4 {
+
+    //Main Method Block
+    public static void main(String[] args) {
+        // Statements to display strings
+        System.out.println("a    a^2    a^3");
+        System.out.println("1    1       1");
+        System.out.println("2    4       8");
+        System.out.println("3    9       27");
+        System.out.println("4    16      64");
+    } //End Main Method Block
+    
+} //End Class Block
